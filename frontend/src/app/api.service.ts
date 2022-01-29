07a20c1321cb01a51adc6d32a89dc7c5d83040f7
LConn <lconn@uwo.ca>
2022-01-29 13:43:50 -0500

    just updating

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -40,4 +40,6 @@ export class ApiService {
     return this.http.post('/answer', body, {headers: headers});
   }
 
+  
+
 }
--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -13,19 +14,18 @@ export class MapComponent implements OnInit {
   lng = -76.4951
   questions: any
 
-  coordinates: Array<[number, number, string]> = [[-76.4951, 44.2253, "Pro-Vax"],
+  answers: Array<[number, number, string]> = [[-76.4951, 44.2253, "Pro-Vax"],
                                                   [-76.4951, 44.25, "Anti-Vax"],
                                                   [-76.4951, 44.3, "Anti-Vax"],
                                                   [-76.4951, 44.4, "Pro-Vax"]];
+
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.auth()
     this.apiService.getQuestions().subscribe((data: any) => {
-      this.questions = data.questions
-      console.log(this.questions)
+      this.questions = data.questions;
     })
   }
-
 }
